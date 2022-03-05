9fa8460dd849a698a3755ad954f7592a3ced9bb7
Ifeanyi Orji <iffychuqs@gmail.com>
2022-03-06 00:29:07 +0100

    up to date

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import LandingPage from "../pages/Screens/LandingPage";
-
+import { EditParty } from "./Screens/create party/edit";
 import PartyDetails from "./Screens/PartyDetails/index";
 import { CreateParty } from "./Screens/create party/index";
+// import ShoutParty from "./Screens/ShoutParty/ShoutParty";
+// import { EditParty } from "./Screens/create party/edit";
 
 export default function Home() {
   return (
     <div>
       {/* <CreateParty /> */}
-      <PartyDetails />
+      {/* <PartyDetails /> */}
+      {/* <EditParty /> */}
+      <LandingPage />
     </div>
   );
 }
