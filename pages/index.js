949fab9adb22c539da72640356da52ee0c96700a
Atolagbe Elisha <atolagbeelisha001>
2022-03-11 08:23:36 +0100

    landing-page removed+correct font load

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+import Router, { useRouter } from "next/router";
+import { useEffect } from "react";
 import LandingPage from "../Screens/LandingPage";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/home");
+  });
   return (
-    <div>
-      <LandingPage />
+    <div style={{ display: "flex", "justify-content": "center" }}>
+      {" "}
+      <CircularProgress color="inherit" />
     </div>
   );
 }
